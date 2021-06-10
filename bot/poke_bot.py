1c13a89d62955c1dae9ce64695b8aa95cf0dc093
Santibruera <santibruera97@gmail.com>
2021-06-10 17:58:41 -0300

    FIX connection

--- bot/poke_bot.py
+++ bot/poke_bot.py
@@ -8,7 +8,8 @@ import database
 
 import os
 
-token = os.getenv("DISCORD_BOT_TOKEN")
+#token = os.getenv("DISCORD_BOT_TOKEN")
+token = 'ODUxOTU0NDQ4NzcwNTMxMzg4.YL_yaQ.KtQy9s7Y5uLPlpWPXR_O-GrKNMw'
 
 
 client = discord.Client()
@@ -24,21 +25,23 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('$poke random'):
-        pokeID = random.randint(1, 800)
-        pokemon = pb.pokemon(pokeID)
-        shiny = random.randint(1, 8192)
-        if shiny == 888:
-            pokePic = pokemon.sprites.front_shiny
+        poke_id = random.randint(1, 898)
+        pokemon = pb.pokemon(poke_id)
+        shiny = random.randint(1, 2048)
+        if shiny == 1024:
+            poke_pic = pokemon.sprites.front_shiny
         else:
-            pokePic = pokemon.sprites.front_default
-        await message.channel.send(pokePic)
+            poke_pic = pokemon.sprites.front_default
+        await message.channel.send(poke_pic)
         await message.channel.send(pokemon.name.capitalize())
     if message.content.startswith('$poke register'):
         user = database.get_user(message.author.id)
-        print(user[0])
         if user is None:
             database.register_user(message.author.id)
             await message.channel.send('Registration Completed! Welcome to the pokemon world')
+        else:
+            await message.channel.send("You\'re already registered")
+
 
 
 client.run(token)
