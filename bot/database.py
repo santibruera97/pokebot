8826a3c51fead2dd2be631e39cc7cf100eb6980e
Santibruera <santibruera97@gmail.com>
2021-06-10 23:02:53 -0300

    First catching system

--- bot/database.py
+++ bot/database.py
@@ -1,8 +1,11 @@
 import psycopg2
 import os
+import datetime;
+
 DATABASE_URL = os.environ['DATABASE_URL']
 
-async def get_user(discord_id):
+
+async def get_user_discord_id(id_user_discord):
     """ query data from the vendors table """
     conn = None
     user = None
@@ -10,7 +13,7 @@ async def get_user(discord_id):
         DATABASE_URL = os.environ['DATABASE_URL']
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor()
-        cur.execute(f"SELECT user_discord_id FROM users WHERE user_discord_id = '{discord_id}'")
+        cur.execute(f"SELECT user_discord_id FROM users WHERE user_discord_id = '{id_user_discord}'")
         user = cur.fetchone()
         cur.close()
 
@@ -21,7 +24,26 @@ async def get_user(discord_id):
             conn.close()
         return user
 
-async def register_user(discord_id):
+async def get_user_id(id_user_discord):
+    """ query data from the vendors table """
+    conn = None
+    user = None
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        cur.execute(f"SELECT user_id FROM users WHERE user_discord_id = '{id_user_discord}'")
+        user = cur.fetchone()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return user
+
+async def register_user(id_user_discord):
     """ insert a new user into the users table """
     sql = """INSERT INTO users(user_discord_id,superballs,ultraballs,masterballs)
                  VALUES(%s,50,25,1) RETURNING user_id;"""
@@ -33,7 +55,7 @@ async def register_user(discord_id):
         # create a new cursor
         cur = conn.cursor()
         # execute the INSERT statement
-        cur.execute(sql, (discord_id,))
+        cur.execute(sql, (id_user_discord))
         # get the generated id back
         user_id = cur.fetchone()[0]
         # commit the changes to the database
@@ -47,3 +69,68 @@ async def register_user(discord_id):
             conn.close()
 
     return user_id
+
+
+async def set_active_pokemon(pokemon_id):
+    ct = datetime.datetime.now()
+    sql = f"UPDATE sys_vars SET var_value= {str(pokemon_id)}, updated_at={ct} WHERE var_name = 'ACTIVE_POKEMON';"
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (str(pokemon_id)))
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+async def insert_pokemon_captured(id_user_discord,id_pokemon):
+    user_id = get_user_id(id_user_discord)
+    sql = f"INSERT INTO user_pokemon(id_user, id_pokemon) VALUES ({user_id}, {id_pokemon});"
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql)
+        await set_active_pokemon('')
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+async def get_active_pokemon():
+    """ query data from the vendors table """
+    conn = None
+    user = None
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        cur.execute(f"SELECT var_value FROM users WHERE var_name = 'ACTIVE_POKEMON'")
+        pokemon_id = cur.fetchone()
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return pokemon_id
--- bot/poke_bot.py
+++ bot/poke_bot.py
@@ -4,7 +4,7 @@ import discord
 import pokebase as pb
 import random
 import database
-import pprint
+import poke_maths
 
 
 import os
@@ -25,9 +25,8 @@ async def on_message(message):
     if message.content.startswith('$poke random'):
         poke_id = random.randint(1, 898)
         pokemon = pb.pokemon(poke_id)
-        print(pokemon.stats)
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(pokemon.stats[0])
+        await database.set_active_pokemon(poke_id)
+
         shiny = random.randint(1, 2048)
         if shiny == 1024:
             poke_pic = pokemon.sprites.front_shiny
@@ -36,13 +35,23 @@ async def on_message(message):
         await message.channel.send(poke_pic)
         await message.channel.send(pokemon.name.capitalize())
     if message.content.startswith('$poke register'):
-        user = await database.get_user(message.author.id)
+        user = await database.get_user_discord_id(message.author.id)
         if user is None:
             await database.register_user(message.author.id)
             await message.channel.send('Registration Completed! Welcome to the pokemon world')
         else:
             await message.channel.send("You\'re already registered")
-
-
+    if message.content.startswith('$pokeball'):
+        try:
+            pokemon_id = int(database.get_active_pokemon())
+            pokemon = pb.pokemon(pokemon_id)
+            result = poke_maths.catch(4)
+            if result == 1:
+                await database.insert_pokemon_captured(message.author.id,pokemon_id)
+                await message.channel.send(f'{message.author.name} You captured a {pokemon.name.capitalize()}')
+            else:
+                await message.channel.send(f'{pokemon.name.capitalize()} Fled away')
+        except Exception as error:
+            print(error)
 
 client.run(token)
