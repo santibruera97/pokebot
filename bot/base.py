a124c0b2a5fbc96579966462d2d14b37a1ce0075
Santibruera <santibruera97@gmail.com>
2021-06-13 00:13:52 -0300

    hotfix falto importacion

--- bot/base.py
+++ bot/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 DATABASE_URL_ALCHEMY = os.environ['DATABASE_URL_ALCHEMY']
 
